ac0bd2492ab88dbed4b1c4c6fa94424609e1312e
Bulwarini <bulwarini11@gmail.com>
2023-10-17 10:48:28 +0200

    podstrona

---
+++ .php_tmp/wydswiat.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="google-adsense-account" content="ca-pub-1518890929804230">
+    <title>Wydarzenia na Świecie - CapForum</title>
+    <link rel="stylesheet" href="mybulma/css/customstyle.css/customstyle.css">
+  </head>
+  <body>
+    <nav class="navbar is-primary" role="navigation" aria-label="main navigation" >
+      <div class="navbar-brand">
+          <img src="obrazki\capforum.svg" height="150" width="150">
+      <div id="navbarBasicExample" class="navbar-menu">
+
+
+        <div class="navbar-start">
+
+          <a class="navbar-item">
+            <strong class="is-size-4">Strona Domowa</strong>
+          </a>
+          <div class="navbar-item has-dropdown is-hoverable">
+
+            <a class="navbar-link">
+              <strong class="is-size-4">Kategorie</strong>
+            </a>
+    
+            <div class="navbar-dropdown is-dark">
+              <a class="navbar-item">
+                Gry
+              </a>
+
+              <a class="navbar-item">
+                Nowinki Technologiczne
+              </a>
+
+              <a class="navbar-item">
+                Software
+              </a>
+
+              <a class="navbar-item">
+                Wydarzenia na świecie
+              </a>
+              
+              <a class="navbar-item">
+                Poradniki
+              </a>
+            </div>
+
+          </div>
+          <a class="navbar-item">
+           Informacje i Kontakt
+          </a>
+        </div>
+      </div>
+      </div>
+
+      <div class="navbar-end">
+        <div class="navbar-item">
+          <div class="buttons">
+            <a class="button is-primary">
+              <strong>Zarejestruj się</strong>
+            </a>
+            <a class="button is-light">
+              Zaloguj się
+            </a>
+          </div>
+        </div>
+      </div>
+
+    </nav>
+
+
+
+
+
+
+  </body>
+
+
+</html>
