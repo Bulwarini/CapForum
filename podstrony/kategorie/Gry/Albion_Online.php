45a299e55a66e0866fb76a6d6bc12464038c02c7
Ciacho <ciastekpl20@gmail.com>
2023-10-20 22:44:04 +0200

    strona albion

---
+++ podstrony/kategorie/Gry/Albion_Online.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="google-adsense-account" content="ca-pub-1518890929804230">
+    <title>CapForum</title>
+    <link rel="stylesheet" href="/mybulma/css/customstyle.css/customstyle.css">
+  </head>
+  <body>
+    
+    <nav class="navbar is-primary" role="navigation" aria-label="main navigation" >
+      <div class="navbar-brand">
+          <img src="\obrazki\capforum.svg" height="150" width="150">
+      <div id="navbarBasicExample" class="navbar-menu">
+
+
+        <div class="navbar-start">
+
+          <a class="navbar-item" href="/">
+            <strong class="is-size-4">Strona Domowa
+
+            </strong>
+          </a>
+          <div class="navbar-item has-dropdown is-hoverable">
+
+            <a class="navbar-link">
+              <strong class="is-size-4">Kategorie</strong>
+            </a>
+    
+            <div class="navbar-dropdown is-dark">
+              <a class="navbar-item" href="/podstrony/kategorie/gry">
+                Gry
+              </a>
+
+              <a class="navbar-item" href="/podstrony/kategorie/nowinkitechno">
+                Nowinki Technologiczne
+              </a>
+
+              <a class="navbar-item" href="/podstrony/kategorie/software">
+                Software
+              </a>
+
+              <a class="navbar-item" href="/podstrony/kategorie/wydswiat">
+                Wydarzenia na świecie
+              </a>
+              
+              <a class="navbar-item" href="/podstrony/kategorie/poradniki">
+                Poradniki
+              </a>
+            </div>
+
+          </div>
+          <a class="navbar-item" href="/podstrony/kategorie/infoikontakt">
+           Informacje i Kontakt
+          </a>
+        </div>
+      </div>
+      </div>
+
+      <div class="navbar-end">
+        <div class="navbar-item">
+          <div class="buttons">
+            <a class="button is-primary" href="/podstrony/konto/register">
+              <strong>Zarejestruj się</strong>
+            </a>
+            <a class="button is-light" href="/podstrony/konto/login">
+              Zaloguj się
+            </a>
+          </div>
+        </div>
+      </div>
+
+    </nav>
+
+    <div class="container" style="align-items: center;">
+      <div class="is section is-large">
+        <div class="box has-background-black-ter is-rounded is-outlined">
+        
+        <h1 class="is title has-text-white has-text-centered has-text-weight-bold">Albion Online</h1><br>
+        <p class="has-text-white is-size- has-text-weight-bold">Albion Online jest sandboksowym MMORPG. Oznacza to, że większość wydarzeń w świecie gry oraz wirtualną ekonomię napędzają działania samych graczy.</p><br>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/_TVA7FFCIVA?si=Crp36r8uUP78XBBm" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+          <p class="has-text-white is-size-4 ">Albion Online to sandbox MMORPG, w którym to Ty piszesz własną historię, zamiast podążać wytyczoną ścieżką. Odkrywaj ogromny otwarty świat. Wszystko, co robisz, wpływa na zależną od gracza ekonomię Albionu. Każdy element ekwipunku jest tworzony przez graczy z surowców, które sami zbierają. Twój ekwipunek decyduje o tym, kim jesteś. Zamiana z rycerza na maga jest tak prosta, jak zmiana zbroi i broni. Możesz też połączyć elementy obu klas.  Ruszaj w głąb lochów, gdzie znajdziesz zarówno bogactwo jak i zagrożenia. Zrelaksuj się na własnej wyspie, na której możesz postawić dom, sadzić rośliny i hodować zwierzęta. Chwyć za wędkę i ruszaj w kierunku jezior, rzek i oceanów Albionu.  Dołącz do gildii, graj z innymi! Bierz udział w ogromnych bitwach PVP i zagarnij dla siebie kawałek świata! To jest Albion Online.</p><br>
+          <p class="has-text-white is-size-1 has-text-weight-bold"> </p>
+          
+          <p class="has-text-white is-size-1 ">Zamknięty otwarty świat</p><br>
+          <p class="has-text-white is-size-4 ">Mapa gry została podzielona na sieć kwadratowych obszarów połączonych ze sobą węzłami. By przejść z jednego rejonu do drugiego musimy udać się na jego skraj, gdzie czeka nas szybki blackscreen i wczytanie sąsiedniego terytorium
+Wspomniane „kwadraty” dzielą się na 4 zasadnicze rodzaje: niebieskie, gdzie PvP jest niedostępne, a nasza postać nie może zginąć (jedynie zostać chwilowo znokautowana); żółte, gdzie PvP jest dostępne, ale – podobnie jak poprzednio – nie możemy zginąć (agresywni gracze jedynie nas nokautują i płacą za to utratą reputacji); czerwone, gdzie PvP jest dostępne, a w wypadku śmierci cały nasz dobytek przepada (a agresor traci reputację); czarne, gdzie PvP jest nieograniczone w żaden z powyższych sposobów (tj. tracimy wszystko, a każdy może nas zaatakować i zabić bez uszczerbku na reputacji).</p>
+<img src="\obrazki\albion\gathering-zones-albion-online-gathering-guide.webp" height="400rem" width="">
+          <p class="is is-size-1"><br></p>
+          <p class="has-text-white is-size-1 ">Recenzje</p><br>
+          <img src="\obrazki\albion\recenzja.png" height="400rem" width="">
+          <img src="\obrazki\albion\recenzja2.png" height="400rem" width="">
+          <img src="\obrazki\albion\recenzja3.png" height="400rem" width="">
+        </div>
+      </div>
+
+    </div>
+  </body>
+
+
+</html>
