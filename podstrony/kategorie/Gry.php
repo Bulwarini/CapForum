83f2f12b1972c1f5ae7550046d8976803f971c4c
Ciacho <ciastekpl20@gmail.com>
2023-10-20 21:23:40 +0200

    Gry v0.1

---
+++ podstrony/kategorie/Gry.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="google-adsense-account" content="ca-pub-1518890929804230">
+    <title>CapForum</title>
+    <link rel="stylesheet" href="/mybulma/css/customstyle.css/customstyle.css">
+  </head>
+  <body>
+    
+    <nav class="navbar is-primary" role="navigation" aria-label="main navigation" >
+      <div class="navbar-brand">
+          <img src="\obrazki\capforum.svg" height="150" width="150">
+      <div id="navbarBasicExample" class="navbar-menu">
+
+
+        <div class="navbar-start">
+
+          <a class="navbar-item" href="/">
+            <strong class="is-size-4">Strona Domowa
+
+            </strong>
+          </a>
+          <div class="navbar-item has-dropdown is-hoverable">
+
+            <a class="navbar-link">
+              <strong class="is-size-4">Kategorie</strong>
+            </a>
+    
+            <div class="navbar-dropdown is-dark">
+              <a class="navbar-item" href="/podstrony/kategorie/gry">
+                Gry
+              </a>
+
+              <a class="navbar-item" href="/podstrony/kategorie/nowinkitechno">
+                Nowinki Technologiczne
+              </a>
+
+              <a class="navbar-item" href="/podstrony/kategorie/software">
+                Software
+              </a>
+
+              <a class="navbar-item" href="/podstrony/kategorie/wydswiat">
+                Wydarzenia na świecie
+              </a>
+              
+              <a class="navbar-item" href="/podstrony/kategorie/poradniki">
+                Poradniki
+              </a>
+            </div>
+
+          </div>
+          <a class="navbar-item" href="/podstrony/kategorie/infoikontakt">
+           Informacje i Kontakt
+          </a>
+        </div>
+      </div>
+      </div>
+
+      <div class="navbar-end">
+        <div class="navbar-item">
+          <div class="buttons">
+            <a class="button is-primary" href="/podstrony/konto/register">
+              <strong>Zarejestruj się</strong>
+            </a>
+            <a class="button is-light" href="/podstrony/konto/login">
+              Zaloguj się
+            </a>
+          </div>
+        </div>
+      </div>
+
+    </nav>
+
+    <div class="container" style="align-items: center;">
+      <div class="is section is-large">
+        <div class="box has-background-black-ter is-rounded is-outlined">
+        <h1 class="is title has-text-white has-text-centered has-text-weight-bold">Gry</h1>
+        <p class="has-text-white is-size-4 ">Albion online</p>
+          <p class="has-text-white is-size-1 has-text-weight-bold">blablablabl</p>
+          <p class="is is-size-1"><br></p>
+        </div>
+      </div>
+
+    </div>
+  </body>
+
+
+</html>
