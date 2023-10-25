d81d6f5d1798219756c04c3ff9fecc8fc8cc3383
IgroPoloniaeCinco <rogalik709@gmail.com>
2023-10-25 17:52:27 +0200

    Artykul europka i zdjecia

---
+++ podstrony/kategorie/katwyd/JWST_Europa.php
@@ -0,0 +1,162 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="google-adsense-account" content="ca-pub-1518890929804230">
+    <title>CapForum</title>
+    <link rel="stylesheet" href="/mybulma/css/customstyle.css/customstyle.css">
+  </head>
+  <body>
+    
+  <nav class="navbar is-primary" role="navigation" aria-label="main navigation" >
+      <div class="navbar-brand">
+          <img src="\obrazki\capforum.svg" height="150" width="150">
+        </div>
+        <script>
+      document.addEventListener('DOMContentLoaded', () => {
+
+            // Get all "navbar-burger" elements
+            const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+
+            // Add a click event on each of them
+            $navbarBurgers.forEach( el => {
+              el.addEventListener('click', () => {
+
+                // Get the target from the "data-target" attribute
+                const target = el.dataset.target;
+                const $target = document.getElementById(target);
+
+                // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+                el.classList.toggle('is-active');
+                $target.classList.toggle('is-active');
+
+              });
+            });
+
+            });
+    </script>
+   
+
+      <div class="contrainer is-flex is-hidden-desktop is-hidden-tablet">
+
+      <div class="navbar-end">
+        <div class="navbar-item is-flex">
+
+          <div class="buttons is-marginless is-justify-items-center">
+            <a class="button is-primary is-marginless" href="/podstrony/konto/register">
+              <strong>Zarejestruj się</strong>
+            </a>
+            <a class="button is-light is-marginless" href="/podstrony/konto/login">
+              Zaloguj się
+            </a>
+            </div>
+        </div>
+      </div>
+
+      <div class="is-flex" style="right:0;position:absolute;">
+          <a role="button" class="navbar-burger is-right" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+      <span aria-hidden="false"></span>
+      <span aria-hidden="true"></span>
+      <span aria-hidden="true"></span>
+          </a>
+        </div>  
+
+      </div>
+      <div id="navbarBasicExample" class="navbar-menu has-background-primary">
+
+<div class="navbar-start">
+
+  <a class="navbar-item" href="/">
+    <strong class="is-size-4">Strona Domowa
+
+    </strong>
+  </a>
+  <div class="navbar-item has-dropdown is-hoverable">
+
+    <a class="navbar-link">
+      <strong class="is-size-4">Kategorie</strong>
+    </a>
+
+    <div class="navbar-dropdown is-dark">
+      <a class="navbar-item" href="/podstrony/kategorie/gry">
+        Gry
+      </a>
+
+      <a class="navbar-item" href="/podstrony/kategorie/nowinkitechno">
+        Nowinki Technologiczne
+      </a>
+
+      <a class="navbar-item" href="/podstrony/kategorie/software">
+        Software
+      </a>
+
+      <a class="navbar-item" href="/podstrony/kategorie/wydswiat">
+        Wydarzenia na świecie
+      </a>
+      
+      <a class="navbar-item" href="/podstrony/kategorie/poradniki">
+        Poradniki
+      </a>
+    </div>
+
+  </div>
+  <a class="navbar-item" href="/podstrony/kategorie/infoikontakt">
+   Informacje i Kontakt
+  </a>
+</div>
+</div>
+<div class="navbar-end is-hidden-mobile">
+        <div class="navbar-item is-flex">
+
+          <div class="buttons is-marginless is-justify-items-center">
+            <a class="button is-primary is-marginless" href="/podstrony/konto/register">
+              <strong>Zarejestruj się</strong>
+            </a>
+            <a class="button is-light is-marginless" href="/podstrony/konto/login">
+              Zaloguj się
+            </a>
+            </div>
+        </div>
+      </div>
+
+    </nav>
+               <div class="container">
+      <div class="is section is-large">
+        <div class="box has-background-black-ter is-rounded is-outlined">
+        <div class="is-flex is-flex-direction-column is-justify-content-center is-align-items-center">
+        <h1 class="is title has-text-white has-text-centered has-text-weight-bold">Kosmiczny Teleskop Jamesa Webba wykrywa źródło węgla na powierzchni księżyca Jowisza, Europy</h1><br>
+        <p class="has-text-white is-size-4 has-text-weight-bold is-align-self-flex-start">Kosmiczny Teleskop Jamesa Webba</p><br>
+          <p class="has-text-white is-size-4 ">Teleskop ten powstawał w latach 2007-2021,
+             na zlecenie NASA. Miał on być dopełnieniem teleskopu Hubble’a. Został on nazwany na cześć drugiego administratora NASA, Jamesa Webba.</p><br>
+             <img src="\obrazki\jwst\jwst.jpg" height="500rem" width="500rem"><br>
+          <p class="has-text-white is-size-4 has-text-weight-bold is-align-self-flex-start">Europa</p>
+          <p class="has-text-white is-size-4 ">Księżyc Jowisza – Europa, jest jednym z niewielu światów w naszym Układzie Słonecznym, który mógłby potencjalnie stwarzać warunki
+             dla życia. Poprzednie badania pokazały, że poniżej swojej lodowej powierzchni, skrywa się ocean ciekłej wody z kamienistym dnem. Jednakże naukowcy nie potwierdzili
+              czy ocean zawierał związki chemiczne potrzebne do życia, szczególnie węgiel. <br><br>Astronomowie posługując się danymi z JWST zidentyfikowali dwutlenek węgla w jednym
+               z regionów na lodowej powierzchni Europy. Analiza wskazuje, że ten węgiel najprawdopodobniej wydobył się z podpowierzchni oceanu i nie został tam dostarczony przez
+                żadne meteory ani inne źródła zewnętrzne. Nota bene znajduje się tam od całkiem niedawna (patrząc na to ze skala geologicznej). To odkrycie wprowadza ważne implikacje
+                 dla potencjalnej zamieszkiwalności europejskiego oceanu.</p><br>
+                 <img src="\obrazki\jwst\Europa.png" height="800rem" width="800rem"><br>
+          <p class="has-text-white is-size-4 has-text-weight-bold is-align-self-flex-start">Połączenie powierzchnia-ocean</p><br>
+          <p class="has-text-white is-size-4">Webb udowadnia, że na powierzchni Europy, dwutlenek węgla jest najbardziej obfity na obszarze nazwanym
+             Tara Regio – geologicznie młody region ogólnie przekształconego terenu znanego jako „obszar chaosu”. Powierzchnia lodu została naruszona
+              i prawdopodobnie dochodziło tam do wymian materiału pomiędzy podpowierzchniowym oceanem, a lodową powierzchnią. <br><br>Naukowcy zidentyfikowali dwutlenek węgla
+               używając danych ze specjalnego modułu teleskopu, służącego do rozkładania światła na czynniki pierwsze (zdjęcia w podczerwieni).
+                To narzędzie zapewnia zakres widma z rozdzielczością 320x320 kilometrów na powierzchni Europy, której średnica wynosi 3128 kilometrów,
+                 pozwalając astronomom na zlokalizowanie odpowiednich związków chemicznych.</p> <br>
+                 <img src="\obrazki\jwst\Europa2.png" height="500rem" width="500rem">
+        </div>
+
+
+
+
+
+          </div>
+            </div>
+    </div>
+    
+  </body>
+
+
+</html>
