36e0630bd859bf876a8010c172972a3df6814256
Ciacho <ciastekpl20@gmail.com>
2023-10-26 23:05:15 +0200

    GUIDE JEST WIELKI

--- .php_tmp/Terraria.php
+++ .php_tmp/Terraria.php
@@ -1,6 +1,11 @@
 <!DOCTYPE html>
 <html>
   <head>
+  <style>
+body {
+  background-image: url('\obrazki\Terraria\back.png');
+}
+</style>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="google-adsense-account" content="ca-pub-1518890929804230">
@@ -9,7 +14,7 @@
     <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1518890929804230"
      crossorigin="anonymous"></script>
   </head>
-  <body>
+  <body> 
     
   <nav class="navbar is-primary" role="navigation" aria-label="main navigation" >
       <div class="navbar-brand">
@@ -124,50 +129,42 @@ $navbarBurgers.forEach( el => {
 
     </nav>
 
+    
     <div class="container" style="align-items: center;">
       <div class="is section is-large">
         <div class="box has-background-black-ter is-rounded is-outlined">
-        <h1 class="is title has-text-white has-text-centered has-text-weight-bold">TERRARIA JAK ZACZĄĆ</h1>
+        <img src="\obrazki\Terraria\TERRARIA.jpg" height="" width="900">
+        <h1 class="is title has-text-white has-text-centered has-text-weight-bold">JAK ZACZĄĆ</h1>
         <div class="has-text-white is-size-4"><a href="/podstrony/kategorie/katpor/Terraria"></a>
 
         </div>
           <p class="has-text-white is-size-1"></p><br>
-          <img src="\obrazki\Terraria\TERRARIA.jpg" height="" width="900">
           <p class="has-text-white is-size-4">ludzie włączają Terrarię i nie wiedzą o co chodzi. Domyślam się, że taka sytuacja zdarza się częściej, po tym jak twórca przypomniał światu o istnieniu tej wspaniałej gierki, wypuszczając znienacka ogromną aktualizację. Wieść ta zabolała mnie na tyle mocno, że postanowiłem choć trochę przyczynić się do poprawy tej niepokojącej sytuacji, pisząc ten mini-poradnik.</p><br>
           <p class="has-text-white is-size-4">Młody terrarysto, jak już zapewne wiesz, Terraria jest grą typu sandbox. Oznacza to, że nie masz narzuconego konkretnego celu, i to, czy zajmiesz się eksploracją świata, czy też budowaniem niesamowitych konstrukcji zależy tylko od ciebie. Problem w tym, że aby w pełni móc docenić swobodę płynącą z rozgrywki, powinieneś wiedzieć, co w ogóle w grze można ciekawego robić – a Terraria nie kwapi się do pomocy graczowi w tym temacie i poza zdawkowymi hasłami wygłaszanymi przez NPC-ów ciężko o jakieś konkretne porady. Przyznam że sam na początku cisnąłem grę w kąt przytłoczony nadmiarem swobody (wiem brzmi absurdalnie) i gdyby nie moje zacięcie do czytania internetowych poradników, nie miałbym dziś 150h na liczniku.</p><br>
-          <p class="has-text-white is-size-1"></p><br>
-          <p class="has-text-white is-size-4"></p><br>
+          <p class="has-text-white is-size-1">W kupie raźniej</p><br>
+          <p class="has-text-white is-size-4">Nawet jeżeli nie tęsknisz za klockami lego i chcesz skupić się wyłącznie na zwiedzaniu, warto abyś poświęcił chwilę na zbudowanie choćby minimalnej siedziby. Może być sklecona z najprostszych materiałów: piachu czy drewna, ważne aby składała się z zamkniętych pokoi (łącznie z ścianą), a każdy z nich zawierał stół, krzesło i źródło światła. Pomocne jest tu menu “housing”, które pozwala sprawdzić czy pokój nadaje się do zamieszkania.</p><br>
+          <img src="\obrazki\Terraria\dom.jpg" height="800rem" width="350rem">
+          <img src="\obrazki\Terraria\domek.png" height="800rem" width="500rem">
+          <p class="has-text-white is-size-4">Pamiętaj aby mieć zawsze wolny pokój, albo najlepiej kilka – przecież nigdy nie wiadomo, kiedy będzie się chciał wprowadzić nowy NPC. Jest to o tyle istotne że NPC są źródłem niektórych przedmiotów, których w żaden inny sposób nie można zdobyć.
+Oprócz tego dobrze jest przeznaczyć jeden z pokoi na składzik – świetnie do tego celu nadają się porozrzucane po świecie skrzynie – można je przytargać do domu, po opróżnieniu ich z zawartości.</p><br>
+          <p class="has-text-white is-size-4">Nie jest to poradnik pt. “jak przeżyć pierwszą noc”, bo tego powstało już naprawdę sporo. Poniżej znajdziesz kilka subiektywnie wybranych elementów gry, na które moim zdaniem warto zwrócić uwagę, a o których istnieniu mogłeś nie wiedzieć. No to jedziemy.</p><br>
+        <p class="has-text-white is-size-2">Rozmiar świata</p><br>
+        <img src="\obrazki\Terraria\swiat.png" height="800rem" width="850rem">
+        <p class="has-text-white is-size-4">Rozpoczęcie eksploracji od razu od świata „Large” może brzmi kusząco, ale uwierz mi, na początku nawet świat „Small” będzie wydawał się ogromny. Dlatego polecam trzymanie swoich pionierskich ambicji na wodzy i start od małego świata. Swoją postać razem z całym majdanem możesz przenosić dowolnie do każdego nowostworzonego świata, więc naprawdę nie ma ciśnienia, aby od razu rzucać się na głęboką wodę.</p><br>
+        <p class="has-text-white is-size-2">Pomoc przy tworzeniu przedmiotów</p><br>
+        <p class="has-text-white is-size-4">Aby wyprawa do trudniejszych obszarów świata była w ogóle możliwa, będziesz potrzebował lepszego oręża niż startowe. I tu pojawia się główny zarzut wobec Terrarii – skąd mam wiedzieć co w ogóle mogę wytworzyć? Otóż z pomocą przychodzi Pan Przewodnik – pierwszy NPC, wraz z którym rozpoczynasz grę. Nie bój się pokazywać mu przedmiotów – nawet tak banalnych jak kawałek drewna – o ile mają w tooltipie słowo “Material”.</p><br>
+        <img src="\obrazki\Terraria\guide.png" height="800rem" width="850rem">
           <p class="has-text-white is-size-2">Wszystko co musisz wiedzieć o King Slime'ie</p><br>
           <p class="has-text-white is-size-4">Każdy boss w grze daje osiągnięcie za jego zabicie... ale jak to zrobić?</p><br>
+          <img src="\obrazki\Terraria\KSlime.png" height="800rem" width="350rem">
           <p class="has-text-white is-size-2">Przygotowanie i przywołanie</p><br>
           <p class="has-text-white is-size-4">King Slime jest najsłabyszym bossem w grze Terraria, jednakże początkującym może sprawić pewien problem. Król szlamu może zostać przyzwany przy pomocy przedmiotu przywołującego go wytwarzanego w demon lub crimson altarze, znajdującym się w podziemiach odpowiadającego im biomu. Do wytworzenia przedmiotu przywoływania potrzebna będzie korona (Wytwarzana z pięciu sztabek platyny lub złota oraz rubiny) i dwudziestu sztuk żelu. Wytworzony tak przedmiot po użyciu przyzwie bossa. Inną opcją na wezwanie wroga, jest zabicie stu pięćdziesięciu slime'ów w czasie trwania deszczu szlamów.
 Poradnik ten celowany jest w większej mierze do początkujących graczy, dlatego zamiast skupiać się na konkretnych klasach, podam najzwyczajniej (moim zdaniem) najłatwiejszy sposób na zabicie tego żelka. Dobrym przygotowaniem do walki z nim, byłoby coś koło dwustu punktów zdrowia, hurtowe ilości granatów i dość długa, płaska przestrzeń.
 By zdobyć granaty na twój świat musi wprowadzić się specjalny npc sprzedający materiały wybuchowe, przyjdzie do jednego z wolnych domów, jeżeli masz jakiekolwiek materiały wybuchowe w ekwipunku (Na początku gry są to bomby, granaty, dynamit) które można zdobyć w skrzyniach znajdujących się w podziemiach. Nie są one specjalnie drogie nawet jak na początek gry, więc kupno ich nie powinno sprawiać wam problemu.
 Jeżeli chcecie możecie przygotować także jakąkolwiek zbroję oraz kilka potionów (Np. leczenia i iron skin potion), ale nie jest to wymagane. Buty jak np. Hermes Boots mogą być przydatne.</p><br>
-<p class="has-text-white is-size-4">Nie jest to poradnik pt. “jak przeżyć pierwszą noc”, bo tego powstało już naprawdę sporo. Poniżej znajdziesz kilka subiektywnie wybranych elementów gry, na które moim zdaniem warto zwrócić uwagę, a o których istnieniu mogłeś nie wiedzieć. No to jedziemy.</p><br>
-<p class="has-text-white is-size-2">Rozmiar świata</p><br>
-<p class="has-text-white is-size-2"></p><br>
-
-
-<div class="dropdown is-hoverable">
-  <div class="dropdown-trigger">
-    <button class="button" aria-haspopup="true" aria-controls="dropdown-menu4">
-      <span>Hover me</span>
-      <span class="icon is-small">
-        <i class="fas fa-angle-down" aria-hidden="true"></i>
-      </span>
-    </button>
-  </div>
-  <div class="dropdown-menu" id="dropdown-menu4" role="menu">
-    <div class="dropdown-content">
-      <div class="dropdown-item">
-        <p>You can insert <strong>any type of content</strong> within the dropdown menu.</p>
-      </div>
-    </div>
-  </div>
-</div>
-
-
+          <p class="has-text-white is-size-5">źródło:</p>
+          <a href="https://steamcommunity.com/sharedfiles/filedetails/?id=2469068466">https://steamcommunity.com/sharedfiles/filedetails/?id=2469068466</a><br>
+          <a href="https://www.gikz.pl/artykuly/wpisy/poradnik-mlodego-terrarysty/">https://www.gikz.pl/artykuly/wpisy/poradnik-mlodego-terrarysty/</a><br>
 
 <p class="has-text-white is-size-1"></p><br>
           <p class="has-text-white is-size-1"></p><br>
