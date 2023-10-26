216a6ea2db1baeb839594ff18fcc83453c582343
IgroPoloniaeCinco <rogalik709@gmail.com>
2023-10-26 13:35:23 +0200

    Artykul-Jowisz, update Europy, link, zdjecia

--- podstrony/kategorie/katwyd/JWST_Jowisz.php
+++ podstrony/kategorie/katwyd/JWST_Jowisz.php
@@ -125,32 +125,29 @@
       <div class="is section is-large">
         <div class="box has-background-black-ter is-rounded is-outlined">
         <div class="is-flex is-flex-direction-column is-justify-content-center is-align-items-center">
-        <h1 class="is title has-text-white has-text-centered has-text-weight-bold">Kosmiczny Teleskop Jamesa Webba wykrywa źródło węgla na powierzchni księżyca Jowisza, Europy</h1><br>
-        <p class="has-text-white is-size-4 has-text-weight-bold is-align-self-flex-start">Kosmiczny Teleskop Jamesa Webba</p><br>
-          <p class="has-text-white is-size-4 ">Teleskop ten powstawał w latach 2007-2021,
-             na zlecenie NASA. Miał on być dopełnieniem teleskopu Hubble’a. Został on nazwany na cześć drugiego administratora NASA, Jamesa Webba.</p><br>
-             <img src="\obrazki\jwst\jwst.jpg" height="500rem" width="500rem"><br>
-          <p class="has-text-white is-size-4 has-text-weight-bold is-align-self-flex-start">Europa</p>
-          <p class="has-text-white is-size-4 ">Księżyc Jowisza – Europa, jest jednym z niewielu światów w naszym Układzie Słonecznym, który mógłby potencjalnie stwarzać warunki
-             dla życia. Poprzednie badania pokazały, że poniżej swojej lodowej powierzchni, skrywa się ocean ciekłej wody z kamienistym dnem. Jednakże naukowcy nie potwierdzili
-              czy ocean zawierał związki chemiczne potrzebne do życia, szczególnie węgiel. <br><br>Astronomowie posługując się danymi z JWST zidentyfikowali dwutlenek węgla w jednym
-               z regionów na lodowej powierzchni Europy. Analiza wskazuje, że ten węgiel najprawdopodobniej wydobył się z podpowierzchni oceanu i nie został tam dostarczony przez
-                żadne meteory ani inne źródła zewnętrzne. Nota bene znajduje się tam od całkiem niedawna (patrząc na to ze skali geologicznej). To odkrycie wprowadza ważne implikacje
-                 dla potencjalnej zamieszkiwalności europejskiego oceanu.</p><br>
-                 <img src="\obrazki\jwst\Europa.png" height="800rem" width="800rem"><br>
-          <p class="has-text-white is-size-4 has-text-weight-bold is-align-self-flex-start">Połączenie powierzchnia-ocean</p><br>
-          <p class="has-text-white is-size-4">Webb udowadnia, że na powierzchni Europy, dwutlenek węgla jest najbardziej obfity na obszarze nazwanym
-             Tara Regio – geologicznie młody region ogólnie przekształconego terenu znanego jako „obszar chaosu”. Powierzchnia lodu została naruszona
-              i prawdopodobnie dochodziło tam do wymian materiału pomiędzy podpowierzchniowym oceanem, a lodową powierzchnią. <br><br>Naukowcy zidentyfikowali dwutlenek węgla
-               używając danych ze specjalnego modułu teleskopu, służącego do rozkładania światła na czynniki pierwsze (zdjęcia w podczerwieni).
-                To narzędzie zapewnia zakres widma z rozdzielczością 320x320 kilometrów na powierzchni Europy, której średnica wynosi 3128 kilometrów,
-                 pozwalając astronomom na zlokalizowanie odpowiednich związków chemicznych.</p> <br>
-                 <img src="\obrazki\jwst\Europa2.png" height="500rem" width="500rem">
+        <h1 class="is title has-text-white has-text-centered has-text-weight-bold">Kosmiczny Teleskop Jamesa Webba bada Jowisza</h1><br>
+          <p class="has-text-white is-size-4 ">JWST odkrył nowe, wcześniej niezaobserwowane zjawisko w atmosferze Jowisza.
+             Prąd strumieniowy, który rozciąga się na prawie 4800 km, spoczywa nad równikiem Jowisza dużo ponad najwyższymi chmurami.
+             Odkrycie tego prądu daje wgląd na to, jak warstwy dynamicznej atmosfery Jowisza oddziałują ze sobą, a także jak dobrze Webb radzi sobie z ich śledzeniem</p><br>
+          <img src="\obrazki\jwst\Jowisz.webp" height="500rem" width="500rem"><br>
+          <p class="has-text-white is-size-4 ">„Pomimo przeróżnych naziemnych teleskopów i Teleskopu Hubble’a obserwujących zmieniającą się Jowiszańską pogodę,
+             Webb dostarczył nowych odkryć na pierścieniach Jowisza, jego satelitach i atmosferze”</p>
+          <p class="has-text-white is-size-6 is-align-self-flex-end"> ~ Imke de Pater (przewodzący zespołu odpowiedzialnego za zdjęcia)</p><br>
+          <p class="has-text-white is-size-4 has-text-weight-bold is-align-self-flex-center">Jowiszański Równikowy Prąd Strumieniowy (Zdjęcie)</p>
+          <img src="\obrazki\jwst\prad_strumieniowy.webp" height="500rem" width="800rem"><br>
+          <p class="has-text-white is-size-4 has-text-weight-bold is-align-self-flex-center">Wiatry Jowisza</p>
+          <img src="\obrazki\jwst\wiatry.webp" height="500rem" width="800rem"><br>
+          <p class="has-text-white is-size-4">Gdy znakomita rozdzielczość i pokrycie elektromagnetycznych fal o różnych długościach pozwoliły Webbowi
+             na wykrycie małych cech chmur użytych do śledzenia prądu, bazowe obserwacje z Teleskopu Hubble’a wykonane jeden dzień po obserwacjach Webb’a
+              także były kluczowe do zidentyfikowania bazowego stanu Jowiszańskiej równikowej atmosfery i do zaobserwowania burz na równiku Jowisza,
+               nie łączących się z prądem strumieniowym.<br><br>
+               „Jowisz posiada skomplikowany, ale powtarzalny schemat wiatrów i temperatur w swojej równikowej stratosferze, wysoko ponad wiatrami w chmurach i mgłach mierzonych
+                na tych długościach fal elektromagnetycznych […] Jeśli siła tego nowego prądu łączy się z oscylującym stratosferycznie schematem, możemy spodziewać się,
+                 że prąd będzie zmieniał się na przestrzeni następnych 2 do 4 lat – bardzo ekscytująca będzie możliwość zbadania tej teorii w nadchodzących latach” </p>
+          <p class="has-text-white is-size-6 is-align-self-flex-end">~ Leigh Fletcher (Członek zespołu z Uniwersytetu Leicesteru w Wielkiej Brytanii)</p>
         </div>
-            <div class="is section has-text-white">Źródła: 
-              <a href="https://www.nasa.gov/solar-system/nasas-webb-finds-carbon-source-on-surface-of-jupiters-moon-europa/">https://www.nasa.gov/solar-system/nasas-webb-finds-carbon-source-on-surface-of-jupiters-moon-europa/;</a><br>
-              <a href="https://webbtelescope.org/contents/media/images/2023/113/01H69ATHSGQBVKCQWVGSFPES3D"> https://webbtelescope.org/contents/media/images/2023/113/01H69ATHSGQBVKCQWVGSFPES3D;</a><br>
-              <a href="https://pl.wikipedia.org/wiki/Kosmiczny_Teleskop_Jamesa_Webba">https://pl.wikipedia.org/wiki/Kosmiczny_Teleskop_Jamesa_Webba </a><br><br>
+            <div class="is section has-text-white">Źródło: 
+              <a href="https://www.nasa.gov/missions/webb/nasas-webb-discovers-new-feature-in-jupiters-atmosphere/">https://www.nasa.gov/missions/webb/nasas-webb-discovers-new-feature-in-jupiters-atmosphere/</a><br><br>
               <p class="has-text-white is-size-4 is-italic">Autor: Igor Rogojsza </p>
             </div>
 
