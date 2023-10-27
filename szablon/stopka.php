a865d97c86d226b8432e0ba6d2e328c44ff55a3a
Bulwarini <bulwarini11@gmail.com>
2023-10-27 17:12:07 +0200

    metalowy kubeczku

--- szablon/stopka.php
+++ szablon/stopka.php
@@ -1,4 +1,4 @@
-<footer class="footer is-primary"  style="position: absolute;left: 0;bottom: 0;width: 100%;">
+<footer class="footer is-primary"  style="position: relative;left: 0;bottom: 0;width: 100%;">
   <div class="content has-text-centered">
     <p class="has-text-white">
       <strong class="has-text-white">CapForum</strong> by CapForumTeam.
