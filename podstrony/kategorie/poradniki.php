0f1534472c687510fe1af5776b579f184e8a4a8a
IgroPoloniaeCinco <rogalik709@gmail.com>
2023-10-27 20:06:52 +0200

    Czerwony napis artykuly na dwoch stronach

--- podstrony/kategorie/poradniki.php
+++ podstrony/kategorie/poradniki.php
@@ -19,7 +19,7 @@ html, body {
     <div class="container" style="align-items: center;">
       <div class="is section is-large">
         <div class="box has-background-black-ter is-rounded is-outlined">
-        <h1 class="is title has-text-white has-text-centered has-text-weight-bold">Poradniki</h1>
+        <h1 class="is title has-text-white has-text-centered has-text-weight-bold">Poradniki</h1><br><h1 class="is title has-text-centered has-text-weight-bold is-size-4" style="color: red;" >Artykuły:</h1>
         <div class="has-text-white is-size-4"><a href="/podstrony/kategorie/katpor/Terraria">Terraria</a>
 
         </div>
