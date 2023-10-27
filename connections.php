aa130416bc5dfef04575d33dcc9516a9a3e7a3e0
Jakub <sabisz.kuba@gmail.com>
2023-10-27 02:23:40 +0200

    PIERDOLENIE

--- connections.php
+++ connections.php
@@ -7,9 +7,4 @@ $dbname = "logowansko";
 // Create connection
 $con = new mysqli($servername, $username, $password, $dbname);
 
-// Check connection
-if ($con->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
-echo "Connected successfully";
 ?>
---
+++ podstrony/kategorie/kattech/14thgenintel.php
@@ -0,0 +1,160 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="google-adsense-account" content="ca-pub-1518890929804230">
+    <title>CapForum</title>
+    <link rel="stylesheet" href="/mybulma/css/customstyle.css/customstyle.css">
+    <style>
+html, body {
+  overflow-x: hidden;  
+}
+</style>
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1518890929804230"
+     crossorigin="anonymous"></script>
+  </head>
+  <body>
+    
+  <nav class="navbar is-primary" role="navigation" aria-label="main navigation" >
+      <div class="navbar-brand">
+          <img src="\obrazki\capforum.svg" height="150" width="150">
+        </div>
+        <script>
+      document.addEventListener('DOMContentLoaded', () => {
+
+// Get all "navbar-burger" elements
+const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+
+// Add a click event on each of them
+$navbarBurgers.forEach( el => {
+  el.addEventListener('click', () => {
+
+    // Get the target from the "data-target" attribute
+    const target = el.dataset.target;
+    const $target = document.getElementById(target);
+
+    // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+    el.classList.toggle('is-active');
+    $target.classList.toggle('is-active');
+
+  });
+});
+
+});
+    </script>
+   
+
+      <div class="contrainer is-flex is-hidden-desktop">
+
+      <div class="navbar-end">
+        <div class="navbar-item is-flex">
+
+          <div class="buttons is-marginless is-justify-items-center">
+            <a class="button is-primary is-marginless" href="/podstrony/konto/register">
+              <strong>Zarejestruj się</strong>
+            </a>
+            <a class="button is-light is-marginless" href="/podstrony/konto/login">
+              Zaloguj się
+            </a>
+            </div>
+        </div>
+      </div>
+
+      <div class="is-flex" style="right:0;position:absolute;">
+          <a role="button" class="navbar-burger is-right" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+      <span aria-hidden="false"></span>
+      <span aria-hidden="true"></span>
+      <span aria-hidden="true"></span>
+          </a>
+        </div>  
+
+      </div>
+      <div id="navbarBasicExample" class="navbar-menu has-background-primary">
+
+<div class="navbar-start">
+
+  <a class="navbar-item" href="/">
+    <strong class="is-size-4">Strona Domowa
+
+    </strong>
+  </a>
+  <div class="navbar-item has-dropdown is-hoverable">
+
+    <a class="navbar-link">
+      <strong class="is-size-4">Kategorie</strong>
+    </a>
+
+    <div class="navbar-dropdown is-dark">
+      <a class="navbar-item" href="/podstrony/kategorie/gry">
+        Gry
+      </a>
+
+      <a class="navbar-item" href="/podstrony/kategorie/nowinkitechno">
+        Nowinki Technologiczne
+      </a>
+
+      <a class="navbar-item" href="/podstrony/kategorie/software">
+        Software
+      </a>
+
+      <a class="navbar-item" href="/podstrony/kategorie/wydswiat">
+        Wydarzenia na świecie
+      </a>
+      
+      <a class="navbar-item" href="/podstrony/kategorie/poradniki">
+        Poradniki
+      </a>
+    </div>
+
+  </div>
+  <a class="navbar-item" href="/podstrony/kategorie/infoikontakt">
+   Informacje i Kontakt
+  </a>
+</div>
+</div>
+<div class="navbar-end is-hidden-mobile is-hidden-tablet">
+        <div class="navbar-item is-flex">
+
+          <div class="buttons is-marginless is-justify-items-center">
+            <a class="button is-primary is-marginless" href="/podstrony/konto/register">
+              <strong>Zarejestruj się</strong>
+            </a>
+            <a class="button is-light is-marginless" href="/podstrony/konto/login">
+              Zaloguj się
+            </a>
+            </div>
+        </div>
+      </div>
+
+    </nav>
+
+    <div class="container" style="align-items: center;">
+      <div class="is section is-large">
+        <div class="box is-rounded is-outlined" style=" background: rgba(20, 19, 19, 0.85)"> 
+
+        <p class="has-text-white is-size-6 ">Autor-Puszkin</p><br>
+        <h1 class="is title has-text-white has-text-centered has-text-weight-bold">Nowe procesory Intela 14 generacji będą dostępne 17 października z aż 6 GHz zegarem </h1><br>
+        <p class="has-text-white is-size- has-text-weight-bold">Intel wydaje swoje procesory 14 generacji w tym tygodniu, obiecując zegary turbo wynoszące 6 GHz po wyjęciu z pudełka w swoim flagowym i9-14900K . Znane również jako Raptor Lake Refresh, Intel zachowuje swoją cene na 14 generację i9, i7 i i5 procesorów w tym roku, trzymając się tego samego cennika jak 13 generacja kiedy nowe czipy będą dostępne 17 października.</p><br>
+        <div class="is-flex is-justify-content-center">
+        <img src="/obrazki/14thgen/kupsztal.png" height="400rem"></img>
+        </div>
+          <p class="has-text-justified has-text-white is-size-4 ">6 GHz turbo nowego i9-14900K czyni go “najszybszym procesorem desktopowym produkowanym seryjnie,” według Intela, nawiązując do specjalnej wersji 13900KS który jako pierwszy przeszedł barierę 6 GHz na domyślnych zegarach w zeszłym roku, ale nie produkowano go seryjnie. Bardziej interesującym procesorem 14 generacji w tym odświerzeniu może być i7-14700K, chociaż, dzięki znaczącemu skokowi w rdzeniach energooszczędnych.</p><br>
+          <p class="has-text-white is-size-1 has-text-weight-bold"> </p>
+          <p class="has-text-white is-size-4 ">W tym roku, Intel zwiększył ilość rdzenii na swoich procesorach i7, z ośmiu rdzenii energooszczędnych i ośmiu rdzenii nastawionych na wydajność do 12 energooszczędnych i ośmiu rdzenii nastawionych na wydajność. To łącznie 20 rdzenii w i7-14700K, blisko 24 rdzenie można znaleźć w i9-14900K. Bazowe taktowanie wynosi na rdzeniach P 3.4GHz  i 2.5GHz na rdzeniach E dla 14700K, takie same jak w zeszłej generacji. Te dodane rdzenie energooszczędne powinny pomóc w pracy kreatywnej i nawet w wydajności gamingowej ala tytułów które kożystają z wielowątkowości.</p>
+<img src="\obrazki\14thgen\kupsztong.png" height="400rem" width="">
+          <p class="is is-size-1"><br></p>
+          <p class="has-text-white is-size-1 ">Benchmarki</p><br>
+          <p class="has-text-white is size 4">Intel ma kilka benchmarków wychodą lepiej AMD Ryzen 9 7950X , ale musimy poczekać i zobaczyć jak wypadnie i7-14700K w porównaniu do AMD Ryzen 9 7950X3D w gamingu</p>
+          <img src="\obrazki\14thgen\bench.png" height="400rem" width="">
+          <p class="has-text-white is-size-5">źródło:</p>
+          <a href="https://www.intel.com/content/www/us/en/newsroom/news/intel-core-14th-gen-desktop-processors.html">https://www.intel.com/content/www/us/en/newsroom/news/intel-core-14th-gen-desktop-processors.html</a><br>
+          <a href="https://download.intel.com/newsroom/2023/client-computing/Intel-Core-14th-Gen-Desktop-Product-Brief.pdf">https://download.intel.com/newsroom/2023/client-computing/Intel-Core-14th-Gen-Desktop-Product-Brief.pdf</a><br>
+        </div>
+      </div>
+
+    </div>
+  </body>
+
+
+</html>
