8f36214c77bacf01e4b97396b7b7f1072c7ff6c3
Bulwarini <bulwarini11@gmail.com>
2023-10-27 03:34:04 +0200

    321

--- podstrony/kategorie/infoikontakt.php
+++ podstrony/kategorie/infoikontakt.php
@@ -156,14 +156,6 @@ $navbarBurgers.forEach( el => {
 
     </div>
 
-    <footer class="footer is-primary"  style="position: fixed;left: 0;bottom: 0;width: 100%;">
-  <div class="content has-text-centered">
-    <p class="has-text-white">
-      <strong class="has-text-white">CapForum</strong> by CapForumTeam.
-      <a href="/podstrony/politykaprywatnosci.php">Polityka Prywatnośći.</a>
-    </p>
-  </div>
-</footer>
 
 
 <footer class="footer is-primary"  style="position: bottom;left: 0;bottom: 0;width: 100%;">
